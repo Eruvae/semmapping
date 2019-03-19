efdf65a475c240277d497c02d960dbf860dbb91d
Tobias Zaenker <tobias.zaenker@aalto.fi>
2019-03-19 16:03:28 +0200

    Adjusted topics for gibsonenv simulation, added some debugging

--- src/mapping.cpp
+++ src/mapping.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <cmath>
 #include <pcl_ros/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
@@ -14,6 +15,8 @@
 #include "hypermap_msgs/SemanticMap.h"
 
 tf2_ros::Buffer tfBuffer;
+
+volatile bool image_received = false;
 
 hypermap_msgs::SemanticMap map;
 sensor_msgs::Image depth;
@@ -33,19 +36,24 @@ double averageDepth(const sensor_msgs::Image &img, const darknet_ros_msgs::Bound
     double depthSum = 0;
     double minDepth = DBL_MAX;
     double maxDepth = -DBL_MAX;
+    size_t px_cnt = 0;
     for (size_t x = box.xmin; x <= box.xmax; x++)
     {
         for (size_t y = box.ymin; y <= box.ymax; y++)
         {
             float dval = depthValue(img, x, y);
-            depthSum += dval;
-            if (dval > maxDepth)
-                maxDepth = dval;
-            if (dval < minDepth)
-                minDepth = dval;
+            if (!std::isnan(dval) && !std::isinf(dval))
+            {
+                depthSum += dval;
+                px_cnt++;
+                if (dval > maxDepth)
+                    maxDepth = dval;
+                if (dval < minDepth)
+                    minDepth = dval;
+            }
         }
     }
-    size_t px_cnt = (box.xmax - box.xmin + 1) * (box.ymax - box.ymin + 1);
+    //size_t px_cnt = (box.xmax - box.xmin + 1) * (box.ymax - box.ymin + 1);
     double avg = depthSum / px_cnt;
 
     double sqr_err = 0;
@@ -54,12 +62,13 @@ double averageDepth(const sensor_msgs::Image &img, const darknet_ros_msgs::Bound
         for (size_t y = box.ymin; y <= box.ymax; y++)
         {
             float dval = depthValue(img, x, y);
-            sqr_err += (dval - avg)*(dval - avg);
+            if (!std::isnan(dval) && !std::isinf(dval))
+                sqr_err += (dval - avg)*(dval - avg);
         }
     }
     double std_div = std::sqrt(sqr_err / (px_cnt - 1));
 
-    std::cout << "Average: " << avg << ", Min: " << minDepth << ", Max: " << maxDepth << ", Std: " << std_div << std::endl;
+    std::cout << "Average: " << avg << ", Min: " << minDepth << ", Max: " << maxDepth << ", Std: " << std_div <<  ", PxCnt: " << px_cnt << std::endl;
 
     return avg;
 }
@@ -67,22 +76,28 @@ double averageDepth(const sensor_msgs::Image &img, const darknet_ros_msgs::Bound
 void receiveDepthImage(const sensor_msgs::ImageConstPtr &img)
 {
     depth = *img;
+    image_received = true;
 }
 
 void receiveBoundingBoxes(const darknet_ros_msgs::BoundingBoxesConstPtr &bx)
 {
     boxes = *bx;
-    for (const darknet_ros_msgs::BoundingBox &box : boxes.bounding_boxes)
+    if (image_received)
     {
-        double avg = averageDepth(depth, box);
+        for (const darknet_ros_msgs::BoundingBox &box : boxes.bounding_boxes)
+        {
+            std::cout << "Bounding box: (" << box.xmin << " | " << box.xmax << "); (" << box.ymin << " | " << box.ymax << ")" << std::endl;
+            std::cout << "Image size: " << depth.width << ", " << depth.height << std::endl;
+            double avg = averageDepth(depth, box);
+        }
     }
 }
 
 void receiveDepthCloud(const sensor_msgs::PointCloud2ConstPtr &cl)
 {
-    sensor_msgs::PointCloud2 cl_out;
+    /*sensor_msgs::PointCloud2 cl_out;
     tfBuffer.transform(*cl, cl_out, map.header.frame_id);
-    pcl::fromROSMsg(cl_out, depthCloud);
+    pcl::fromROSMsg(cl_out, depthCloud);*/
 }
 
 void pclExtractPoints(const pcl::PointCloud<pcl::PointXYZ> depthCloud, const darknet_ros_msgs::BoundingBox &box)
@@ -122,9 +137,13 @@ int main(int argc, char **argv)
 
   tf2_ros::TransformListener tfListener(tfBuffer);
 
-  ros::Subscriber depthImageSub = nh.subscribe("/camera/depth/image", 100, receiveDepthImage);
-  ros::Subscriber boundingBoxSub = nh.subscribe("/darknet/bounding_boxes", 100, receiveBoundingBoxes);
-  ros::Subscriber depthCloudSub = nh.subscribe("/sensorring_cam3d_front/depth/points", 100, receiveDepthCloud);
+  /*ros::Subscriber depthImageSub = nh.subscribe("/camera/depth/image", 100, receiveDepthImage);
+  ros::Subscriber boundingBoxSub = nh.subscribe("/darknet_ros/bounding_boxes", 100, receiveBoundingBoxes);
+  ros::Subscriber depthCloudSub = nh.subscribe("/sensorring_cam3d_front/depth/points", 100, receiveDepthCloud);*/
+
+  ros::Subscriber depthImageSub = nh.subscribe("/gibson_ros/camera/depth/image", 100, receiveDepthImage);
+  ros::Subscriber boundingBoxSub = nh.subscribe("/darknet_ros/bounding_boxes", 100, receiveBoundingBoxes);
+  ros::Subscriber depthCloudSub = nh.subscribe("/gibson_ros/camera/depth_registered/points", 100, receiveDepthCloud);
 
   ros::spin();
 }
